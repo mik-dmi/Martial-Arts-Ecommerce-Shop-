a78e6d3a1e6f61ec77462a33cbcb92c9fd1fdb71
mik-dmi <122227240+mik-dmi@users.noreply.github.com>
2023-12-05 19:08:02 +0000

    Cart page and Order Sumerry are built and funtion

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -40,11 +40,13 @@ export default function Navbar() {
           ))}
         </nav>
         <div className="flex divide-x border-r sm:border-l">
-          <Button variant={"outline"} className = "flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">
-            <ShoppingBag />
-            <span className="ml-2 text-sm font-bold">{cartCount}</span>
-            <span className="hidden text-xs font-semibold text-gray-500 sm:block">Card</span>
-          </Button>
+          <Link href="/cart">
+            <Button variant={"outline"} className = "flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">
+              <ShoppingBag />
+              <span className="ml-2 text-sm font-bold">{cartCount}</span>
+              <span className="hidden text-xs font-semibold text-gray-500 sm:block">Card</span>
+            </Button>
+          </Link>
         </div>
       </div>
     </header>
